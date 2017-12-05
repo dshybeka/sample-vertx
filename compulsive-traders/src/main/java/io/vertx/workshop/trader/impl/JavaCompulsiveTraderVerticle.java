4032f9800514ae7e32226b4693216df7a4ce27e9
dshybeka <dshybeka@eharmony.com>
2017-12-05 15:10:44 +0300

    Before chapter 10

--- compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/JavaCompulsiveTraderVerticle.java
+++ compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/JavaCompulsiveTraderVerticle.java
@@ -18,10 +18,32 @@ public class JavaCompulsiveTraderVerticle extends MicroServiceVerticle {
   public void start(Future<Void> future) {
     super.start();
 
-    //TODO
-    //----
-    future.fail("no implemented yet...");
-    // ----
+    String company = TraderUtils.pickACompany();
+    int numberOfShares = TraderUtils.pickANumber();
+
+    System.out.println("Java compulsive trader configured for company " + company + " and shares: " + numberOfShares);
+
+      Future<MessageConsumer<JsonObject>> marketFuture = Future.future();
+      Future<PortfolioService> portfolioFuture = Future.future();
+
+      MessageSource.getConsumer(discovery, new JsonObject().put("name", "market-data"), marketFuture.completer());
+      EventBusService.getProxy(discovery, PortfolioService.class, portfolioFuture.completer());
+
+      CompositeFuture.all(marketFuture, portfolioFuture).setHandler(ar -> {
+
+          if(ar.succeeded()) {
+
+              PortfolioService portfolioService = portfolioFuture.result();
+              MessageConsumer<JsonObject> messageConsumer = marketFuture.result();
+              messageConsumer.handler(markedDataHolder -> TraderUtils.dumbTradingLogic(company, numberOfShares, portfolioService, markedDataHolder.body()));
+
+              future.complete();
+          } else {
+
+              future.fail(ar.cause());
+          }
+
+      });
   }
 
 
