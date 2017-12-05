644fb0931ef7ead846bc11293111ae8d41a23f37
dshybeka <dshybeka@eharmony.com>
2017-12-05 18:40:03 +0300

    Done

--- trader-dashboard/src/main/java/io/vertx/workshop/dashboard/DashboardVerticle.java
+++ trader-dashboard/src/main/java/io/vertx/workshop/dashboard/DashboardVerticle.java
@@ -3,7 +3,9 @@ package io.vertx.workshop.dashboard;
 import io.vertx.circuitbreaker.CircuitBreaker;
 import io.vertx.circuitbreaker.CircuitBreakerOptions;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -17,96 +19,141 @@ import io.vertx.servicediscovery.rest.ServiceDiscoveryRestEndpoint;
 import io.vertx.servicediscovery.types.HttpEndpoint;
 import io.vertx.workshop.common.MicroServiceVerticle;
 
+import java.util.function.Function;
+
 /**
  * The dashboard of the micro-trader application.
  */
 public class DashboardVerticle extends MicroServiceVerticle {
 
-  private CircuitBreaker circuit;
-  private WebClient client;
+    private CircuitBreaker circuit;
+    private WebClient client;
 
-  @Override
-  public void start(Future<Void> future) {
-    super.start();
-    Router router = Router.router(vertx);
+    @Override
+    public void start(Future<Void> future) {
+        super.start();
+        Router router = Router.router(vertx);
 
-    // Event bus bridge
-    SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
-    BridgeOptions options = new BridgeOptions();
-    options
-        .addOutboundPermitted(new PermittedOptions().setAddress("market"))
-        .addOutboundPermitted(new PermittedOptions().setAddress("portfolio"))
-        .addOutboundPermitted(new PermittedOptions().setAddress("service.portfolio"))
-        .addInboundPermitted(new PermittedOptions().setAddress("service.portfolio"))
-        .addOutboundPermitted(new PermittedOptions().setAddress("vertx.circuit-breaker"));
+        // Event bus bridge
+        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+        BridgeOptions options = new BridgeOptions();
+        options
+                .addOutboundPermitted(new PermittedOptions().setAddress("market"))
+                .addOutboundPermitted(new PermittedOptions().setAddress("portfolio"))
+                .addOutboundPermitted(new PermittedOptions().setAddress("service.portfolio"))
+                .addInboundPermitted(new PermittedOptions().setAddress("service.portfolio"))
+                .addOutboundPermitted(new PermittedOptions().setAddress("vertx.circuit-breaker"));
 
-    sockJSHandler.bridge(options);
-    router.route("/eventbus/*").handler(sockJSHandler);
+        sockJSHandler.bridge(options);
+        router.route("/eventbus/*").handler(sockJSHandler);
 
-    // Discovery endpoint
-    ServiceDiscoveryRestEndpoint.create(router, discovery);
+        // Discovery endpoint
+        ServiceDiscoveryRestEndpoint.create(router, discovery);
 
-    // Last operations
-    router.get("/operations").handler(this::callAuditService);
+        // Last operations
+        router.get("/operations").handler(this::callAuditServiceWithExceptionHandlerWithCircuitBreaker);
 
-    // Static content
-    router.route("/*").handler(StaticHandler.create());
+        // Static content
+        router.route("/*").handler(StaticHandler.create());
 
-    // Create a circuit breaker.
-    circuit = CircuitBreaker.create("http-audit-service", vertx,
-        new CircuitBreakerOptions()
-            .setMaxFailures(2)
-            .setFallbackOnFailure(true)
-            .setResetTimeout(2000)
-            .setTimeout(1000))
-        .openHandler(v -> retrieveAuditService());
+        // Create a circuit breaker.
+        circuit = CircuitBreaker.create("http-audit-service", vertx,
+                new CircuitBreakerOptions()
+                        .setMaxFailures(2)
+                        .setFallbackOnFailure(true)
+                        .setResetTimeout(2000)
+                        .setTimeout(1000))
+                .openHandler(v -> retrieveAuditService());
 
-    vertx.createHttpServer()
-        .requestHandler(router::accept)
-        .listen(8080, ar -> {
-          if (ar.failed()) {
-            future.fail(ar.cause());
-          } else {
-            retrieveAuditService();
-            future.complete();
-          }
-        });
-  }
-
-  @Override
-  public void stop() throws Exception {
-    if (client != null) {
-      client.close();
+        vertx.createHttpServer()
+                .requestHandler(router::accept)
+                .listen(8080, ar -> {
+                    if (ar.failed()) {
+                        future.fail(ar.cause());
+                    } else {
+                        retrieveAuditService();
+                        future.complete();
+                    }
+                });
     }
-    circuit.close();
-  }
 
-  private Future<Void> retrieveAuditService() {
-    return Future.future(future -> {
-      HttpEndpoint.getWebClient(discovery, new JsonObject().put("name", "audit"), client -> {
-        this.client = client.result();
-        future.handle(client.map((Void)null));
-      });
-    });
-  }
-
-
-  private void callAuditService(RoutingContext context) {
-    if (client == null) {
-      context.response()
-          .putHeader("content-type", "application/json")
-          .setStatusCode(200)
-          .end(new JsonObject().put("message", "No audit service").encode());
-    } else {
-      client.get("/").send(ar -> {
-        if (ar.succeeded()) {
-          HttpResponse<Buffer> response = ar.result();
-          context.response()
-              .putHeader("content-type", "application/json")
-              .setStatusCode(200)
-              .end(response.body());
+    @Override
+    public void stop() throws Exception {
+        if (client != null) {
+            client.close();
         }
-      });
+        circuit.close();
     }
-  }
+
+    private Future<Void> retrieveAuditService() {
+        return Future.future(future -> {
+            HttpEndpoint.getWebClient(discovery, new JsonObject().put("name", "audit"), client -> {
+                this.client = client.result();
+                future.handle(client.map((Void) null));
+            });
+        });
+    }
+
+
+    private void callAuditService(RoutingContext context) {
+        if (client == null) {
+            context.response()
+                    .putHeader("content-type", "application/json")
+                    .setStatusCode(200)
+                    .end(new JsonObject().put("message", "No audit service").encode());
+        } else {
+            client.get("/").send(ar -> {
+                if (ar.succeeded()) {
+                    HttpResponse<Buffer> response = ar.result();
+                    context.response()
+                            .putHeader("content-type", "application/json")
+                            .setStatusCode(200)
+                            .end(response.body());
+                }
+            });
+        }
+    }
+
+    private void callAuditServiceWithExceptionHandler(RoutingContext context) {
+
+        if (client == null) {
+            context.response()
+                    .putHeader("content-type", "application/json")
+                    .setStatusCode(200)
+                    .end(new JsonObject().put("message", "No audit service").encode());
+        } else {
+            client.get("/")
+                    .timeout(config().getInteger("http.timeout", 5000)).send(ar -> {
+                if (ar.succeeded()) {
+                    HttpResponse<Buffer> response = ar.result();
+                    context.response()
+                            .exceptionHandler(throwable -> context.fail(throwable))
+                            .putHeader("content-type", "application/json")
+                            .setStatusCode(200)
+                            .end(response.body());
+                } else {
+
+                    context.fail(ar.cause());
+                }
+            });
+        }
+    }
+
+    private void callAuditServiceWithExceptionHandlerWithCircuitBreaker(RoutingContext context) {
+
+        HttpServerResponse resp = context.response()
+                .putHeader("content-type", "application/json")
+                .setStatusCode(200);
+
+        circuit.executeWithFallback(future -> client.get("/")
+                .timeout(config().getInteger("http.timeout", 5000))
+                .send(ar -> {
+                    if (ar.succeeded()) {
+                        future.complete(ar.result().body());
+                    } else {
+                        future.fail(ar.cause());
+                    }
+                }), throwable -> Buffer.buffer("{\"message\":\"No audit service, or unable to call it\"}"))
+                .setHandler(ar -> resp.end(ar.result()));
+    }
 }
